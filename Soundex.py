f41148dd0eccaf25b558610c221fe643016a6838
AVINASH SHIVANAND KUMBAR <84611916+Avinashsk2000@users.noreply.github.com>
2024-08-30 15:12:33 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -15,19 +15,10 @@ def generate_soundex(name):
     if not name:
         return ""
 
-    # Start with the first letter (capitalized)
-    soundex = name[0].upper()
-    prev_code = get_soundex_code(soundex)
+    soundex = name[0].upper() + ''.join(
+        get_soundex_code(char) for char in name[1:]
+        if (code := get_soundex_code(char)) != '0' and code != soundex[-1]
+    )
 
-    for char in name[1:]:
-        code = get_soundex_code(char)
-        if code != '0' and code != prev_code:
-            soundex += code
-            prev_code = code
-        if len(soundex) == 4:
-            break
+    return (soundex[:4] if len(soundex) >= 4 else soundex.ljust(4, '0'))
 
-    # Pad with zeros if necessary
-    soundex = soundex.ljust(4, '0')
-
-    return soundex
