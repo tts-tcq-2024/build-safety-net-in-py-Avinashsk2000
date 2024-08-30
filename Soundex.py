7d2af58e5b533cfd72ba8db8f11fe10e303654ca
AVINASH SHIVANAND KUMBAR <84611916+Avinashsk2000@users.noreply.github.com>
2024-08-30 15:13:45 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -11,14 +11,25 @@ def get_soundex_code(c):
     return mapping.get(c, '0')  # Default to '0' for non-mapped characters
 
 
+
+You said:
 def generate_soundex(name):
     if not name:
         return ""
 
-    soundex = name[0].upper() + ''.join(
-        get_soundex_code(char) for char in name[1:]
-        if (code := get_soundex_code(char)) != '0' and code != soundex[-1]
-    )
+    # Start with the first letter (capitalized)
+    soundex = name[0].upper()
+    prev_code = get_soundex_code(soundex)
 
-    return (soundex[:4] if len(soundex) >= 4 else soundex.ljust(4, '0'))
+    for char in name[1:]:
+        code = get_soundex_code(char)
+        if code != '0' and code != prev_code:
+            soundex += code
+            prev_code = code
+        if len(soundex) == 4:
+            break
 
+    # Pad with zeros if necessary
+    soundex = soundex.ljust(4, '0')
+
+    return soundex
