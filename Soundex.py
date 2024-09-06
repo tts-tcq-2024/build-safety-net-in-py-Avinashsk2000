5bbf7700d69fb0fd9f793d85fdf3c361958596d4
AVINASH SHIVANAND KUMBAR <84611916+Avinashsk2000@users.noreply.github.com>
2024-09-06 15:44:34 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -1,6 +1,7 @@
 from itertools import groupby
 
 def get_soundex_code(c):
+    """Map characters to Soundex codes."""
     c = c.upper()
     mapping = {
         'B': 1, 'F': 1, 'P': 1, 'V': 1,
@@ -12,7 +13,7 @@ def get_soundex_code(c):
     }
     return mapping.get(c, 0)
 
-def filter_non_alpha(char):
+def filter_non_alphabet(char):
     """Filter out non-alphabetic characters."""
     return char.isalpha()
 
@@ -24,17 +25,20 @@ def remove_consecutive_duplicates(codes):
     """Remove consecutive duplicate Soundex codes."""
     return [key for key, _ in groupby(codes)]
 
-def generate_soundex(name):
-    if not name:
-        return "0000"
-
-    # Apply the processing steps
-    filtered_name = filter(filter_non_alpha, name[1:])
+def process_name(input):
+    """Process the input by filtering, mapping, and removing duplicates."""
+    filtered_name = filter(filter_non_alphabet, name[1:])
     mapped_codes = map(map_to_soundex_code, filtered_name)
     filtered_codes = filter(lambda x: x != 0, mapped_codes)
-    unique_codes = remove_consecutive_duplicates(filtered_codes)
+    return remove_consecutive_duplicates(filtered_codes)
 
-    # Build the final Soundex code
-    return (
-        name[0].upper() + ''.join(map(str, unique_codes))
-    )[:4].ljust(4, '0')
+def build_soundex_code(name, unique_codes):
+    """Build the final Soundex code with the first letter and mapped Soundex codes."""
+    return (name[0].upper() + ''.join(map(str, unique_codes)))[:4].ljust(4, '0')
+
+def generate_soundex(name):
+    """Generate the Soundex code for a given name."""
+    if not name:
+        return "0000"
+    unique_codes = process_name(input)
+    return build_soundex_code(name, unique_codes)
