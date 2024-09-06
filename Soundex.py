db52960b6252866fd1e3dc1230fbe1e6524668ee
AVINASH SHIVANAND KUMBAR <84611916+Avinashsk2000@users.noreply.github.com>
2024-09-06 15:46:31 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -27,18 +27,18 @@ def remove_consecutive_duplicates(codes):
 
 def process_name(input):
     """Process the input by filtering, mapping, and removing duplicates."""
-    filtered_name = filter(filter_non_alphabet, name[1:])
+    filtered_name = filter(filter_non_alphabet, input[1:])
     mapped_codes = map(map_to_soundex_code, filtered_name)
     filtered_codes = filter(lambda x: x != 0, mapped_codes)
     return remove_consecutive_duplicates(filtered_codes)
 
-def build_soundex_code(name, unique_codes):
+def build_soundex_code(input, unique_codes):
     """Build the final Soundex code with the first letter and mapped Soundex codes."""
-    return (name[0].upper() + ''.join(map(str, unique_codes)))[:4].ljust(4, '0')
+    return (input[0].upper() + ''.join(map(str, unique_codes)))[:4].ljust(4, '0')
 
-def generate_soundex(name):
+def generate_soundex(input):
     """Generate the Soundex code for a given name."""
-    if not name:
+    if not input:
         return "0000"
     unique_codes = process_name(input)
-    return build_soundex_code(name, unique_codes)
+    return build_soundex_code(input, unique_codes)
